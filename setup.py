a777c8f821411ecaa38e8ef60bfa7ffda5b22683
Dan Watson <dcwatson@gmail.com>
2014-01-13 12:01:13 -0500

    Fixed distutils packaging, don't require Django for client usage

--- setup.py
+++ setup.py
@@ -1,31 +1,48 @@
 from distutils.core import setup
-import tally
 import os
 
-def path_parts(path):
-    parts = []
-    while True:
-        head, tail = os.path.split(path)
-        parts.insert(0, tail)
-        if not head:
-            break
-        path = head
-    return parts
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join)
+    in a platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
 
 packages = []
 package_data = {}
-tally_dir = os.path.join(os.path.dirname(__file__), 'tally')
-for path, dirnames, filenames in os.walk(tally_dir):
-    parts = path_parts(path)
+
+# This is necessary so os.walk will return relative paths.
+root_dir = os.path.dirname(__file__)
+if root_dir:
+    os.chdir(root_dir)
+
+for dirpath, dirnames, filenames in os.walk('tally'):
+    # Ignore PEP 3147 cache dirs and those whose names start with '.'
+    dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
+    parts = fullsplit(dirpath)
+    package_name = '.'.join(parts)
     if '__init__.py' in filenames:
-        package_name = '.'.join(parts)
         packages.append(package_name)
     elif filenames:
-        package_data.setdefault(parts[0], []).extend([os.path.join(*(parts[1:] + [f])) for f in filenames])
+        relative_path = []
+        while '.'.join(parts) not in packages:
+            relative_path.insert(0, parts.pop())
+        path = os.path.join(*relative_path)
+        package_files = package_data.setdefault('.'.join(parts), [])
+        package_files.extend([os.path.join(path, f) for f in filenames])
+
+version = __import__('tally').__version__
 
 setup(
     name='tally',
-    version=tally.__version__,
+    version=version,
     description='A Django application (and daemon) for collecting, storing, and aggregating metrics.',
     author='Dan Watson',
     author_email='dcwatson@gmail.com',
--- tally/__init__.py
+++ tally/__init__.py
@@ -1,9 +1,14 @@
-__version_info__ = (0, 1, 0)
+__version_info__ = (0, 2, 0)
 __version__ = '.'.join(str(i) for i in __version_info__)
 
-from django.conf import settings
 import socket
 import time
+
+try:
+    from django.conf import settings
+except ImportError:
+    # Don't require Django if only using the UDP client.
+    settings = None
 
 TALLY_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
